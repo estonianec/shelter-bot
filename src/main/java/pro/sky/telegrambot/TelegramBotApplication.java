c4b21fd1e4eaeaf23b00b616e37e04b5576b202d
FrostMind <Priimak98@Gmail.com>
2022-05-18 16:00:42 +1000

    123

--- src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
+++ src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
@@ -10,6 +10,7 @@ public class TelegramBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotApplication.class, args);
+		System.out.println("!");
 	}
 
 }
