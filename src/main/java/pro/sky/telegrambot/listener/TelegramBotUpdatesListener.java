82d7613d365b3239249043a37efce58d5b6a0b20
estonec <rusecru@gmail.com>
2022-05-25 10:06:51 +0300

    Добавил в листенер обработку команды /start

--- src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
+++ src/main/java/pro/sky/telegrambot/listener/TelegramBotUpdatesListener.java
@@ -3,6 +3,7 @@ package pro.sky.telegrambot.listener;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,8 @@ import java.util.List;
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
-    private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
+    private Long chatId;
+    private final Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
 
     @Autowired
     private TelegramBot telegramBot;
@@ -28,7 +30,18 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
     public int process(List<Update> updates) {
         updates.forEach(update -> {
             logger.info("Processing update: {}", update);
-            // Process your updates here
+            if (update.message() != null && update.message().text() != null) {
+                String msg = update.message().text();
+                chatId = update.message().chat().id();
+                String name = update.message().from().firstName();
+                switch (msg) {
+                    case "/start":
+                        SendMessage msgForSend = new SendMessage(chatId, "test \uD83E\uDEE0");
+                        telegramBot.execute(msgForSend);
+                        break;
+                    default:
+                }
+            }
         });
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
