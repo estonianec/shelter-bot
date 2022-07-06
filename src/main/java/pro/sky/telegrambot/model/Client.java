4fb620e25aced6497136c41eee8b8d8677dd6844
estonec <rusecru@gmail.com>
2022-07-06 07:20:38 +0300

    Исправить модель client добавив поле животного и Изменить испытательные сроки на 14 и 30 дней

--- src/main/java/pro/sky/telegrambot/model/Client.java
+++ src/main/java/pro/sky/telegrambot/model/Client.java
@@ -16,9 +16,18 @@ public class Client {
     LocalDateTime adoptionDate; // Дата взятия животного
     LocalDateTime probationDate; // Дата окончания испытательного срока
     int adoptionStatus; //0 - без животного, 1 - на испытательном сроке, 2 - усыновление принято, 3 - в усыновлении отказано
+    int animalType; //0 - животное не выбрано, 1 - собака, 2 - кошка
 
     public Client() {
 
+    }
+
+    public int getAnimalType() {
+        return animalType;
+    }
+
+    public void setAnimalType(int animalType) {
+        this.animalType = animalType;
     }
 
     public int getAdoptionStatus() {
